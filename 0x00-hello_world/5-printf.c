76e938fd18cdcd3d296b9f9bfaac1ce5a7f63c76
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-05 21:47:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - With this programme, we  will print a line using the printf function
- * return 0 (code succesfully executed)
+ * Return: 0 (code succesfully executed)
  */
 int main(void)
 {
