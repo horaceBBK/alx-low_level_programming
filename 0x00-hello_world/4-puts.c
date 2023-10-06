69625ed78719ece5f72e163579831b28b8677324
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-05 20:20:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program will print a line with functions
+ * Return: 0 (Code successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multiingual puzzle");
+return (0);
+}
