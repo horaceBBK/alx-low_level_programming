c2f91cc3575ca99ca36ac0a876135cccaa367b1b
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-05 21:56:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,7 @@
  * main - With this programme, we  will print a line using the printf function
  * Return: 0 (code succesfully executed)
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
