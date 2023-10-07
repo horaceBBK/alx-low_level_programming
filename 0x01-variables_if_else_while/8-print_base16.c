4e1f0661b6f19e20754e830a40b0354ed72a2f53
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-07 16:43:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - the  programs entry point
+ * Return 0 (code successful)
+ */
+
+int main(void)
+{
+	char x;
+
+	int y;
+
+	x = 'a';
+	y = 0;
+	while
+		(y < 10) {
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(x <= 'f') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
