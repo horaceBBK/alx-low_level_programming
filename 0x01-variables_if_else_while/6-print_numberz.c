925c77d92da680395453d946431d5d06e6784867
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-07 16:21:33 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the programs entry point
+ * Return: 0 (code successful)
+ */
+
+int main(void)
+{
+	int g;
+
+	g = 0;
+	while
+		(g < 10) {
+			putchar(g + '0');
+			g++;
+		}
+	putchar('\n');
+	return (0);
+}
