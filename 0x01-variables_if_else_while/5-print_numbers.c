5c7e1043817b3444fa32b2e874696a8d8a9d5c1d
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-07 16:09:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: 0 (code successful)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		printf("%d", m);
+	putchar('\n');
+	return (0);
+}
