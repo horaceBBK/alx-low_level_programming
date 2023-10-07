e108c4c22804ad0942028ad59d19e87635d73e7d
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-07 00:20:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The code's entry point
+ * Return: 0 (code successful)
+ */
+
+int main(void)
+{
+	char f;
+
+	f = 'a';
+	while
+		(f <= 'z') {
+			if ((f != 'q' && f != 'c') && f <= 'z')
+				putchar(f);
+			f++;
+		}
+	putchar('\n');
+	return (0);
+}
