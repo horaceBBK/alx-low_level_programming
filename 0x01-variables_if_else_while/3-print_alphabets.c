19a6d99db838b163a935287970387c497e31b700
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-06 23:55:57 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Return: 0 (code successful)
+ */
+
+int main(void)
+{
+	char v;
+
+	char w;
+
+	v = 'a';
+	w = 'A';
+	while
+		(v <= 'z') {
+			putchar(v);
+			v++;
+		}
+	while
+		(w <= 'Z') {
+			putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
