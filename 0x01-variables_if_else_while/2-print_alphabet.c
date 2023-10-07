fb7b8587341e2fcae87f93d11756d556fa16f556
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-06 23:40:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Return: 0 (Code successful)
+ */
+
+int main(void)
+{
+	char w;
+
+	w = 'a';
+	while
+		(w <= 'z') {
+			putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
