96eea11c4ee1c0ed51d00ab37a6de00bbe4286ea
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-07 16:29:15 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the programs entry point
+ * Return: 0 (code successful)
+ */
+
+int main(void)
+{
+	char m;
+
+	m = 'z';
+	while
+		(m >= 'a') {
+			putchar(m);
+			m--;
+		}
+	putchar('\n');
+	return (0);
+}
