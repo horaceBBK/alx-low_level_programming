8b77cf2730dd2489e65aec86adc9613f4cc786b7
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-07 22:07:07 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: 0 (Code successful)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015--\n", 59);
+}
