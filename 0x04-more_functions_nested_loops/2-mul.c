94d98c9216189295845ec4fb7db7d1fe3741e6b8
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-13 17:46:09 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * ml - multiply 2 int 
+ * @a: first int
+ * @b: second int
+ *
+ * Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
