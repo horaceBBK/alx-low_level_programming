1a89fc2a1c8e651b952da9f87edade1b98770746
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-30 21:38:39 -0700

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,37 +2,38 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenate two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to the concatenated string
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
  *
- * Description: This function concatenates two strings and returns
- * a new string which is the result of concatenating s1 and s2.
+ * Return: a pointer to a 2 dimensional array of integers.
  */
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-    char *concat_str;
-    int i, j, len1 = 0, len2 = 0;
+	int **array, i, j;
 
-    if (s1 != NULL)
-        while (s1[len1] != '\0')
-            len1++;
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    if (s2 != NULL)
-        while (s2[len2] != '\0')
-            len2++;
+	array = malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
 
-    concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (concat_str == NULL)
-        return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			while (i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
 
-    for (i = 0; i < len1; i++)
-        concat_str[i] = s1[i];
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
 
-    for (j = 0; j < len2; j++)
-        concat_str[i + j] = s2[j];
-
-    concat_str[len1 + len2] = '\0';
-    return (concat_str);
+	return (array);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * free_grid - frees a 2-dimensional array of integers.
- * @grid: the 2-dimensional array of integers to be freed.
- * @height: height of the grid.
- *
- * Return: Nothing.
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid
+ * @height: height of the grid
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	}
 	free(grid);
 }
