83946c7089a25ba7165414d9b43e5d6112503742
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-10-30 21:26:46 -0700

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,40 +12,27 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int i, concat_index;
+    char *concat_str;
+    int i, j, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
+    if (s1 != NULL)
+        while (s1[len1] != '\0')
+            len1++;
 
-	if (s2 == NULL)
-		s2 = "";
+    if (s2 != NULL)
+        while (s2[len2] != '\0')
+            len2++;
 
-	i = concat_index = 0;
-	while (s1[i] != '\0')
-		i++;
+    concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (concat_str == NULL)
+        return (NULL);
 
-	while (s2[concat_index] != '\0')
-		concat_index++;
+    for (i = 0; i < len1; i++)
+        concat_str[i] = s1[i];
 
-	concat_str = malloc(sizeof(char) * (i + concat_index + 1));
+    for (j = 0; j < len2; j++)
+        concat_str[i + j] = s2[j];
 
-	if (concat_str == NULL)
-		return (NULL);
-
-	i = concat_index = 0;
-	while (s1[i] != '\0')
-	{
-		concat_str[i] = s1[i];
-		i++;
-	}
-
-	while (s2[concat_index] != '\0')
-	{
-		concat_str[i] = s2[concat_index];
-		i++, concat_index++;
-	}
-
-	concat_str[i] = '\0';
-	return (concat_str);
+    concat_str[len1 + len2] = '\0';
+    return (concat_str);
 }
