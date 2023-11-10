f7f014041503eee7b72ea1556964f878567d25b8
Eric Jäger-Asare <ericjasare@gmail.com.com>
2023-11-10 14:32:21 -0800

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,19 +10,19 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-        int i = 0;
+	int i = 0;
 
-        if (size > 0)
-        {
-                if (array != NULL && cmp != NULL)
-                {
-                        while (i < size)
-                        {
-                                if (cmp(array[i]))
-                                        return (i);
-                                i++;
-                        }
-                }
-        }
-        return (-1);
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+	return (-1);
 }
