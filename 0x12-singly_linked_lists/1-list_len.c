368621b58593a2b7ee2cb133f2058199d4d61ed9
Eric Jäger-Asare <ericjasare@gmail.com>
2023-11-16 22:06:35 -0800

    Updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - find the length of a list
- * @h: head node
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: length of node
+ * Return: number of elements in h
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		n++;
 		h = h->next;
-		nodes++;
 	}
-	return (nodes);
+	return (n);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,27 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @str: string to store
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Return: pointer to the new node, or NULL if failed
+ * Return: the address of the new element, or NULL if it fails
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode;
-	unsigned int i, count = 0;
+	list_t *new;
+	unsigned int len = 0;
 
-	newnode = malloc(sizeof(list_t));
-	if (newnode == NULL)
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
-	newnode->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++)
-		count++;
-	newnode->len = count;
-	newnode->next = *head;
-	*head = newnode;
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
